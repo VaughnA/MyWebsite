07b897900eee9d594f04826c842b17418698c660
VaughnA <vaughn_arjavac@davincischools.org>
2017-12-08 15:46:34 -0800

    Create movies2.js

---
+++ movies2.js
@@ -0,0 +1,10 @@
+function myFunction() {
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+//https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_toggle_hide_show//
