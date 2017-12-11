f3d00dc7cb43cd10e69f7fc60905cc070d0393c1
VaughnA <vaughn_arjavac@davincischools.org>
2017-12-10 20:52:48 -0800

    Update movies2.js

--- movies2.js
+++ movies2.js
@@ -1,10 +1,9 @@
-function myFunction() {
-    var x = document.getElementById("myDIV");
+function myFunction(elem) {
+    var x = document.getElementById("commentary");
     if (x.style.display === "none") {
         x.style.display = "block";
     } else {
         x.style.display = "none";
     }
 }
-
 //https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_toggle_hide_show//
